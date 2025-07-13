bb73f07472d3993e69a74db574994e543c45668e
badrGADI <badr.gadi.123@gmail.com>
2025-07-13 23:59:39 +0200

    last_our-products

--- app/sitemap.ts
+++ app/sitemap.ts
@@ -51,6 +51,12 @@ export default function sitemap(): MetadataRoute.Sitemap {
       priority: 0.6,
     },
     {
+      url: "https://www.the-3rocks.com/our-products/acopper",
+      lastModified: new Date(),
+      changeFrequency: "monthly",
+      priority: 0.6,
+    },
+    {
       url: "https://www.the-3rocks.com/articles/acopper",
       lastModified: new Date(),
       changeFrequency: "monthly",
