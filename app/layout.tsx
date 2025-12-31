cc146d8932ec2f553368dd899c606ec0d7841fc9
badrGADI <badr.gadi.123@gmail.com>
2025-12-31 06:21:37 +0100

    feat: Create root layout with global styling, fonts, and GA integration.

--- app/layout.tsx
+++ app/layout.tsx
@@ -8,6 +8,7 @@ import { Inter, Red_Hat_Display } from "next/font/google";
 import RootLayoutClient from "./root-layout-client";
 import Header from "@/components/ui/header";
 import Footer from "@/components/ui/footer";
+import Script from "next/script";
 
 const inter = Inter({
   subsets: ["latin"],
@@ -39,6 +40,19 @@ export default function RootLayout({
       <body
         className={`${inter.variable} ${redhat.variable} font-inter antialiased bg-white text-gray-900 dark:bg-gray-900 dark:text-gray-100 tracking-tight`}
       >
+        <Script
+          src="https://www.googletagmanager.com/gtag/js?id=G-0QV4CTYT36"
+          strategy="afterInteractive"
+        />
+        <Script id="google-analytics" strategy="afterInteractive">
+          {`
+            window.dataLayer = window.dataLayer || [];
+            function gtag(){dataLayer.push(arguments);}
+            gtag('js', new Date());
+
+            gtag('config', 'G-0QV4CTYT36');
+          `}
+        </Script>
         <RootLayoutClient>
           <div className="flex flex-col min-h-screen overflow-hidden">
             <Header />
